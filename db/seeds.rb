3e42608c0fb7dc311ad4cf0f3d909ae382c171dd
Jurjen Munneke <jurjenmunneke@me.com>
2016-06-01 09:38:15 +0200

    Done with the task manager

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Task.create(name: "Diner", description: "Buy groceries for the meal", complete: false)
+Task.create(name: "Bike", description: "Repair the wheel of my bike", complete: false)
+Task.create(name: "Rails", description: "Learn ruby on rails", complete: true)
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
